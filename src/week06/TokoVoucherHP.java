8f2cee787789b33db046e56db271cbc3e1182fcc
erickdjaja <erick.djaja@gmail.com>
2025-10-06 22:41:59 +0700

    Initial commit

---
+++ src/week06/TokoVoucherHP.java
@@ -0,0 +1,14 @@
+package week06;
+
+public class TokoVoucherHP {
+
+	public TokoVoucherHP() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
